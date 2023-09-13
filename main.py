7c4fb0d5cec2d0bc9f50f3bc992f054b4b4f7301
farhantanvir1 <34395767+farhantanvir1@users.noreply.github.com>
2023-09-13 12:23:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,13 +19,92 @@ from torch.optim import Adam
 import argparse
 import numpy as np
 from sklearn.metrics import recall_score, precision_score, roc_auc_score, accuracy_score, f1_score, average_precision_score, precision_recall_curve
-from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
 from torch_geometric.data import Data
 from torch.utils.data import DataLoader, TensorDataset
 from torch_geometric.utils import subgraph
-from torch.utils.data import DataLoader, TensorDataset
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 import time
+from sklearn.metrics import ndcg_score
+
+def hit_at_n(predictions, labels, n=10):
+    """
+    Compute the Hit@N metric.
+
+    :param predictions: Predicted scores, numpy array of shape (num_samples, num_classes).
+    :param labels: True labels, numpy array of shape (num_samples, num_classes).
+    :param n: The cutoff for computing Hit@N.
+
+    :return: Hit@N score.
+    """
+    num_samples = predictions.shape[0]
+    num_negative_samples = 100  # Number of negative samples per test triplet
+
+    # Get the indices of the top-N predictions for each sample
+    top_n_preds = np.argsort(predictions, axis=1)[:, -n:]
+
+    hit = 0
+    for i in range(num_samples):
+        sample_labels = np.concatenate((labels[i], np.random.choice(labels.shape[1], size=num_negative_samples)))
+        if np.any(sample_labels[top_n_preds[i]]):
+            hit += 1
+
+    return hit / num_samples
+
+
+def dcg_at_k(y_true, y_score, n):
+    """
+    Compute DCG@k for predictions.
+
+    :param y_true: True values, numpy array of bools.
+    :param y_score: Predicted scores, numpy array of floats.
+    :param k: The cutoff for computing DCG.
+
+    :return score: DCG@k score
+    """
+    # First, we sort the true values by the predicted score (descending order)
+    order = np.argsort(y_score)[::-1]
+    y_true_sorted = np.take(y_true, order[:n])
+
+    # Compute the DCG@k: sum of true values / log2(rank + 1)
+    gain = 2 ** y_true_sorted - 1
+    discounts = np.log2(np.arange(len(y_true_sorted)) + 2)
+    return np.sum(gain / discounts)
+
+def ndcg_at_k(y_true, y_score, n):
+    """
+    Compute nDCG@k for predictions.
+
+    :param y_true: True values, numpy array of bools.
+    :param y_score: Predicted scores, numpy array of floats.
+    :param k: The cutoff for computing nDCG.
+
+    :return score: nDCG@k score
+    """
+    dcg_max = dcg_at_k(y_true, y_true, n)
+    if not dcg_max:
+        return 0.
+    dcg = dcg_at_k(y_true, y_score, n)
+    return dcg / dcg_max
+
+
+
+
+def remove_test_edges(hetero_graph, test_edges):
+    remaining_edges = hetero_graph.edge_index.t().tolist()
+    test_edges = test_edges.tolist()
+
+    # Remove test edges from the list of remaining edges
+    remaining_edges = [edge for edge in remaining_edges if edge not in test_edges]
+
+    # Convert the remaining edges back to the edge_index format
+    remaining_edges = torch.tensor(remaining_edges, dtype=torch.long).t()
+
+    # Create a new hetero_graph with the remaining edges
+    hetero_graph_no_test = Data(x=hetero_graph.x, edge_index=remaining_edges).to(hetero_graph.x.device)
+
+    return hetero_graph_no_test
+
 
 def main(args):
     # If args['hetero'] is True, g would be a heterogeneous graph.
@@ -110,11 +189,11 @@ def main(args):
           epoch_loss += loss.item()
 
       # Print average loss for the current epoch 
-      print(f'Graph Triplet Attention Network training: time elapsed= {(time.time() - time_start):.2f} [s] | Epoch= {epoch + 1} | Loss= {epoch_loss/ len(X_train)}')
+      print(f'HeTriNet training: Epoch= {epoch + 1} | Loss= {epoch_loss/ len(X_train)}')
 
     # Print average loss for the current epoch
     #print(f'HTN Epoch: {epoch + 1}, Loss: {epoch_loss / len(X_train)}')
-
+    torch.save(model.state_dict(), '/content/drive/MyDrive/HTN-main 3/model.pth')
     model.eval()
     correct = 0
     total = 0
@@ -143,15 +222,48 @@ def main(args):
     y_true = np.array(y_true_list)
     y_pred = np.array(y_pred_list)
 
+    # Compute F1 Score
+    f1 = f1_score(y_true, (y_pred > 0.5).astype(int))
+
+    # Compute Recall and Precision
+    recall = recall_score(y_true, (y_pred > 0.5).astype(int))
+    precision = precision_score(y_true, (y_pred > 0.5).astype(int))
+
     # Compute ROC AUC score
     roc_auc = roc_auc_score(y_true, y_pred)
 
     # Compute AUPR score
     aupr = average_precision_score(y_true, y_pred)
-
+  
+    # Print F1 Score, Recall, and Precision
+    print(f"F1 Score: {f1 * 100:.2f}%")
+    print(f"Recall: {recall * 100:.2f}%")
+    print(f"Precision: {precision * 100:.2f}%")
     print(f"ROC AUC: {roc_auc* 100:.2f}%")
     print(f"AUPR: {aupr* 100:.2f}%")
+
+    # Compute Hit@n and NDCG@n
+    n = 5  # Adjust this value according to your needs
+    hit_n = hit_at_n(y_pred, y_true, n=n)
+    ndcg_n = ndcg_at_k(y_true, y_pred, n=n)
+    print(f"Hit@{n}: {hit_n * 100:.2f}%")
+    print(f"NDCG@{n}: {ndcg_n * 100:.2f}%")
+
+    n = 10  # Adjust this value according to your needs
+    hit_n = hit_at_n(y_pred, y_true, n=n)
+    ndcg_n = ndcg_at_k(y_true, y_pred, n=n)
+    print(f"Hit@{n}: {hit_n * 100:.2f}%")
+    print(f"NDCG@{n}: {ndcg_n * 100:.2f}%")
+
+    n = 15  # Adjust this value according to your needs
+    hit_n = hit_at_n(y_pred, y_true, n=n)
+    ndcg_n = ndcg_at_k(y_true, y_pred, n=n)
+    print(f"Hit@{n}: {hit_n * 100:.2f}%")
+    print(f"NDCG@{n}: {ndcg_n * 100:.2f}%")
+
     print(f'Test accuracy: {accuracy * 100:.2f}%')
+
+    
 
 
 if __name__ == '__main__':
